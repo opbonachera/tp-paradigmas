9858f388f106e61e8a08b38d73786ba3c5516ff9
Ornella Bonachera <obonachera@gmail.com>
2026-06-07 17:56:52 -0300

    Agrego clase Factory para la clase Hechizos

---
+++ MagosVsMortifagosGrupoAlpha/src/Hechizos/FactoryPattern.java
@@ -0,0 +1,15 @@
+package Hechizos;
+
+public class FactoryPattern {
+	
+	public Hechizo crearHechizo(String tipo) {
+        switch (tipo.toLowerCase()) {
+            case "avadakedavra":    return new AvadaKedavra();
+            case "expectopatronum": return new ExpectoPatronum();
+            case "expelliarmus":    return new Expelliarmus();
+            case "protego":         return new Protego();
+            default:                throw new IllegalArgumentException("Tipo de hechizo desconocido: " + tipo);
+        }
+    }
+	
+}
